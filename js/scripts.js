7f4827593c3afd936b2b665c0799497069051d77
Mustafa Ghasemi <mostafa_ghasemi63@yahoo.com>
2019-04-05 11:47:04 -0700

    add the second for loop to check each number index

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,43 @@
+// function fizzBuzz(userInput){
+//   var userNumber = "";
+//   for (var i = 0; i <= userInput; i++) {
+//     var num = i;
+//     newNum=num.toString();
+//     //console.log(newNum[1]);
+//     if(newNum[0] === "3" || newNum[1] === "3" || newNum[2] === "3"){
+//       console.log("hi");
+//       userNumber += ("I'm sorry, Dave. I'm afraid I can't do that.");
+//     }else if(newNum[0] === "2" || newNum[1] === "2" || newNum[2] === "2"){
+//       userNumber += ("Boop");
+//     }else if(newNum[0] === "1" || newNum[1] === "1" || newNum[2] === "1"){
+//       userNumber += ("Beep");
+//     }else {
+//       userNumber += i ;
+//     }
+//
+//   }
+//   return userNumber;
+// }
+
+
+
 function fizzBuzz(userInput){
   var userNumber = "";
   for (var i = 0; i <= userInput; i++) {
     var num = i;
     newNum=num.toString();
-    //console.log(newNum[1]);
-    if(newNum[0] === "3" || newNum[1] === "3" || newNum[2] === "3"){
+    for (var j = 0; j < newNum.length; j++) {
+    if(newNum[j] === "3"){
       console.log("hi");
       userNumber += ("I'm sorry, Dave. I'm afraid I can't do that.");
-    }else if(newNum[0] === "2" || newNum[1] === "2" || newNum[2] === "2"){
+    }else if(newNum[j] === "2"){
       userNumber += ("Boop");
-    }else if(newNum[0] === "1" || newNum[1] === "1" || newNum[2] === "1"){
+    }else if(newNum[j] === "1"){
       userNumber += ("Beep");
     }else {
-      userNumber += i;
+      userNumber += i ;
+      }
     }
-
   }
   return userNumber;
 }
@@ -29,6 +52,7 @@ $(document).ready(function(){
     var userInput = $("input#number").val();
     var finalResult = fizzBuzz(userInput);
     $("#result").show();
+
     $("#result").append(finalResult);
   });
 });
