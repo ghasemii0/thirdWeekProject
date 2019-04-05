d1e1aaf25f3aff875033f43691f7d6973f5836bc
Mustafa Ghasemi <mostafa_ghasemi63@yahoo.com>
2019-04-05 13:31:38 -0700

    finish if statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,21 @@
-// function fizzBuzz(userInput){
-//   var userNumber = "";
-//   for (var i = 0; i <= userInput; i++) {
-//     var num = i;
-//     newNum=num.toString();
-//     //console.log(newNum[1]);
-//     if(newNum[0] === "3" || newNum[1] === "3" || newNum[2] === "3"){
-//       console.log("hi");
-//       userNumber += ("I'm sorry, Dave. I'm afraid I can't do that.");
-//     }else if(newNum[0] === "2" || newNum[1] === "2" || newNum[2] === "2"){
-//       userNumber += ("Boop");
-//     }else if(newNum[0] === "1" || newNum[1] === "1" || newNum[2] === "1"){
-//       userNumber += ("Beep");
-//     }else {
-//       userNumber += i ;
-//     }
-//
-//   }
-//   return userNumber;
-// }
-
-
-
 function fizzBuzz(userInput){
   var userNumber = "";
   for (var i = 0; i <= userInput; i++) {
     var num = i;
     newNum=num.toString();
-    for (var j = 0; j < newNum.length; j++) {
-    if(newNum[j] === "3"){
-      userNumber += ("I'm sorry, Dave. I'm afraid I can't do that.");
-
-    }else if(newNum[j] === "2"){
-      userNumber += ("Boop");
-
-    }else if(newNum[j] === "1"){
-      userNumber += ("Beep");
-
+    //console.log(newNum[1]);
+    if(newNum[0] === "3" || newNum[1] === "3" || newNum[2] === "3"){
+      userNumber += ("I'm sorry, Dave. I'm afraid I can't do that.<br>");
+    }else if(newNum[0] === "2" || newNum[1] === "2" || newNum[2] === "2"){
+      userNumber += ("Boop<br>");
+    }else if(newNum[0] === "1" || newNum[1] === "1" || newNum[2] === "1"){
+      userNumber += ("Beep<br>");
     }else {
-      userNumber += i ;
-      }
+      userNumber += i + "<br>";
     }
   }
   return userNumber;
 }
-
-
-
 
 
 $(document).ready(function(){
