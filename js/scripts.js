f71423c520223d9350f3dc9b4881a4d190d8b80d
Mustafa Ghasemi <mostafa_ghasemi63@yahoo.com>
2019-04-05 14:20:30 -0700

    work on if statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,68 @@
+// function fizzBuzz(userInput){
+//   var userNumber = "";
+//   for (var i = 0; i <= userInput; i++) {
+//     var num = i;
+//     newNum=num.toString();
+//     //console.log(newNum[1]);
+//     if(newNum[0] === "3" || newNum[1] === "3" || newNum[2] === "3"){
+//       userNumber += ("I'm sorry, Dave. I'm afraid I can't do that.<br>");
+//     }else if(newNum[0] === "2" || newNum[1] === "2" || newNum[2] === "2"){
+//       userNumber += ("Boop<br>");
+//     }else if(newNum[0] === "1" || newNum[1] === "1" || newNum[2] === "1"){
+//       userNumber += ("Beep<br>");
+//     }else {
+//       userNumber += i + "<br>";
+//     }
+//   }
+//   return userNumber;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function fizzBuzz(userInput){
   var userNumber = "";
+  var num = "";
+
   for (var i = 0; i <= userInput; i++) {
-    var num = i;
-    newNum=num.toString();
-    //console.log(newNum[1]);
-    if(newNum[0] === "3" || newNum[1] === "3" || newNum[2] === "3"){
-      userNumber += ("I'm sorry, Dave. I'm afraid I can't do that.<br>");
-    }else if(newNum[0] === "2" || newNum[1] === "2" || newNum[2] === "2"){
-      userNumber += ("Boop<br>");
-    }else if(newNum[0] === "1" || newNum[1] === "1" || newNum[2] === "1"){
-      userNumber += ("Beep<br>");
-    }else {
-      userNumber += i + "<br>";
-    }
+     num += i;
+    // }else if(newNum[0] === "2" || newNum[1] === "2" || newNum[2] === "2"){
+    //   userNumber += ("Boop<br>");
+    // }else if(newNum[0] === "1" || newNum[1] === "1" || newNum[2] === "1"){
+    //   userNumber += ("Beep<br>");
+    // }else {
+    //   userNumber += i + "<br>";
+    // }
   }
+  var newNum=num.split("");
+  console.log(newNum);
+  newNum.forEach(function(n) {
+    if([3].includes(newNum)){
+      userNumber += ("I'm sorry, Dave. I'm afraid I can't do that.<br>");
+}
   return userNumber;
 }
+
+}
+
+
+
+
+
+
 
 
 $(document).ready(function(){
